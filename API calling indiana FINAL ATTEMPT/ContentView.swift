1c10b0b3480070c1a41e7a51051fa4e4b175e6ec
Evan Zona <evanbzona@gmail.com>
2022-02-18 14:18:26 -0600

    got API key

--- API calling indiana FINAL ATTEMPT/ContentView.swift
+++ API calling indiana FINAL ATTEMPT/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var info = [content]()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(info) { content in
+                NavigationLink(
+                    destination: Text(content.artist)
+                        .padding(),
+                    label: {
+                        Text(content.title)
+                    })
+            }
+            .navigationTitle("Billboard Hot 100")
+            
+        }
+        .onAppear(perform: {
+            getinfo()
+        })
+    }
+    func getinfo() {
+        let apiKey = "ccbe24b966msh00f19017580b48bp12c43fjsn104ce1c9f318"
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct content: Identifiable {
+    let id = UUID()
+    var title: String
+    var artist: String
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+            
+        }
     }
 }
