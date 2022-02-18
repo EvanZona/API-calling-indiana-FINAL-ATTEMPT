269641d7f7957501ff32baa4197b1d787ffea3de
Evan Zona <evanbzona@gmail.com>
2022-02-18 13:45:38 -0600

    Initial Commit

---
+++ API calling indiana FINAL ATTEMPT/API_calling_indiana_FINAL_ATTEMPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_calling_indiana_FINAL_ATTEMPTApp.swift
+//  API calling indiana FINAL ATTEMPT
+//
+//  Created by Student on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct API_calling_indiana_FINAL_ATTEMPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
