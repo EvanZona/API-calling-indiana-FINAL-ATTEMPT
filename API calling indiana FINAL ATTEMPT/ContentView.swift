8665bd41caff824f71e1012d2f2bf0205eb3ad67
Evan Zona <evanbzona@gmail.com>
2022-03-31 11:01:29 -0500

    added app icon + made data display correctly

--- API calling indiana FINAL ATTEMPT/ContentView.swift
+++ API calling indiana FINAL ATTEMPT/ContentView.swift
@@ -8,37 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var info = [content]()
+    @State private var singles = [Single]()
+    @State private var showingAlert = false
     var body: some View {
         NavigationView {
-            List(info) { content in
+            List(singles) {single in
                 NavigationLink(
-                    destination: Text(content.artist)
-                        .padding(),
+                    destination: VStack {
+                        Text(single.rank)
+                        Text(single.title)
+                        Text(single.artist)
+                        //Text("Last week: \(single.lastWeek)")
+                        //Text("Peak position: \(single.peakPosition)")
+                        //Text("Weeks on chart: \(single.weeksOnChart)")
+                        //Text("Detail: \(single.detail)")
+                    },
                     label: {
-                        Text(content.title)
+                        HStack {
+                            Text(single.rank)
+                            Text(single.title)
+                            Text(single.artist)
+                        }
                     })
             }
-            .navigationTitle("Billboard Hot 100")
+            .navigationTitle("Top 10 songs")
             
         }
         .onAppear(perform: {
-            getinfo()
+            queryAPI()
+        })
+        .alert(isPresented: $showingAlert, content: {
+            Alert(title: Text("Loading Error"),
+                  message: Text("There was a problem loading the data"),
+                  dismissButton: .default(Text("OK")))
         })
     }
-    func getinfo() {
-        let apiKey = "ccbe24b966msh00f19017580b48bp12c43fjsn104ce1c9f318"
+    
+    func queryAPI() {
+        let apiKey = "&rapidapi-key=ccbe24b966msh00f19017580b48bp12c43fjsn104ce1c9f318"
+        let query = "https://billboard-api2.p.rapidapi.com/hot-100?date=2021-02-20&range=1-10\(apiKey)"
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                let content = json["content"].dictionaryValue
+                for i in 1...10{
+                    if let item = content[String(i)]?.dictionaryValue {
+                        let rank = item["rank"]!.stringValue
+                        let title = item["title"]!.stringValue
+                        let artist = item["artist"]!.stringValue
+                        //let lastWeek = item["Last week"].stringValue
+                        //let peakPosition = item["peak position"].stringValue
+                        //let detail = item["detail"].stringValue
+                        let single = Single(rank: rank, title: title, artist: artist)
+                        singles.append(single)
+                    }
+                }
+            }
+        }
     }
-}
-struct content: Identifiable {
-    let id = UUID()
-    var title: String
-    var artist: String
+    
+    struct Single: Identifiable {
+        let id = UUID()
+        var rank: String
+        var title: String
+        var artist: String
+    }
     
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             ContentView()
-            
         }
     }
 }
